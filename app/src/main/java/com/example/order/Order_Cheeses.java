3d8c23f9791c20ab8141d947343b77846f6054dd
DULANJALI <it20641788@my.sliit.lk>
2022-03-23 03:54:16 +0530

    This is Order interface in our 'Koththuwa' app

---
+++ app/src/main/java/com/example/order/Order_Cheeses.java
@@ -0,0 +1,14 @@
+package com.example.order;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Order_Cheeses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_chicken);
+    }
+}
